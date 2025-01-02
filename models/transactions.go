9b349629f4a6733d1892ebab92d2081888153ffc
Dnreikronos <dnreikronos2@gmail.com>
2025-01-02 17:14:30 -0400

    refactor: change description to string in transactrions input struct

--- models/transactions.go
+++ models/transactions.go
@@ -15,8 +15,8 @@ type Transactions struct {
 }
 
 type TransactionsInput struct {
-	Description int64 `json:"description" binding:"required,gt=0"`
-	Value       int64 `json:"value" binding:"required,gt=0"`
+	Description string `json:"description" binding:"required,gt=0"`
+	Value       int64  `json:"value" binding:"required,gt=0"`
 }
 
 func (t *Transactions) BeforeCreate(d *gorm.DB) (err error) {
