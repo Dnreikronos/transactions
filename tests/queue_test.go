e61b32c3aec5e9e4db40c0e84e96296eeafacb66
Dnreikronos <dnreikronos2@gmail.com>
2025-01-04 13:30:30 -0400

    tests: create queue tests

---
+++ tests/queue_test.go
@@ -0,0 +1,31 @@
+package tests
+
+import (
+	"testing"
+	"time"
+
+	"github.com/Dnreikronos/transactions/queue"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConsumeQueue(t *testing.T) {
+	message := []byte(`{"description": "Test transaction", "value": 100.00, "date": "2025-01-02"}`)
+
+	processedMessages := make(chan []byte, 1)
+
+	process := func(msg []byte) {
+		processedMessages <- msg
+	}
+
+	go queue.ConsumeQueue(process)
+
+	err := queue.PublishToQueue(message)
+	assert.NoError(t, err, "Expected message to be published to the queue")
+
+	select {
+	case msg := <-processedMessages:
+		assert.Equal(t, message, msg, "Expected processed message to match the original")
+	case <-time.After(3 * time.Second):
+		t.Fatal("Expected message to be processed within the time limit")
+	}
+}
