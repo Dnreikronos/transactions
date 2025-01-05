83dd0f1e9f8198102e5b8a5ae7e7c77088b3e0f8
Dnreikronos <dnreikronos2@gmail.com>
2025-01-05 11:25:35 -0400

    feat: add currency in transaction and transactionInput struct

--- models/transactions.go
+++ models/transactions.go
@@ -12,16 +12,17 @@ type Transactions struct {
 	Description string    `json:"description" gorm:"size:50;not null"`
 	Value       float64   `json:"value" gorm:"not null;check:value > 0"`
 	Date        time.Time `json:"date" gorm:"not null"`
+	Currency    string    `json:"currency" gorm:"size:3;not null"`
 }
 
 type TransactionsInput struct {
 	Description string  `json:"description" binding:"required,max=50"`
 	Value       float64 `json:"value" binding:"required,gt=0"`
 	Date        string  `json:"date" binding:"required,datetime=2006-01-02"`
+	Currency    string  `json:"currency" binding:"required,max=3"`
 }
 
 func (t *Transactions) BeforeCreate(d *gorm.DB) (err error) {
 	t.ID = uuid.New()
-
 	return
 }
