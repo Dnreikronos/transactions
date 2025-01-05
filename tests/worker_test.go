7d31addd7e20fb5f78dae9a03d6735899575cbfb
Dnreikronos <dnreikronos2@gmail.com>
2025-01-05 11:24:29 -0400

    test: add currency in worker test

--- tests/worker_test.go
+++ tests/worker_test.go
@@ -14,9 +14,8 @@ import (
 )
 
 func TestWorkerProcessesTransaction(t *testing.T) {
-
 	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
-	assert.NoError(t, err, "Expected database connection to succeed")
+	assert.NoError(t, err)
 
 	db.AutoMigrate(&models.Transactions{})
 
@@ -26,20 +25,22 @@ func TestWorkerProcessesTransaction(t *testing.T) {
 		Description: "Test transaction",
 		Value:       100.00,
 		Date:        time.Now(),
+		Currency:    "USD",
 	}
 	transactionJSON, err := json.Marshal(transaction)
-	assert.NoError(t, err, "Expected JSON marshaling to succeed")
+	assert.NoError(t, err)
 
 	err = queue.PublishToQueue(transactionJSON)
-	assert.NoError(t, err, "Expected transaction to be published to the queue")
+	assert.NoError(t, err)
 
 	time.Sleep(1 * time.Second)
 
 	var savedTransaction models.Transactions
 	err = db.First(&savedTransaction).Error
 
-	assert.NoError(t, err, "Expected transaction to be saved in the database")
+	assert.NoError(t, err)
 	assert.Equal(t, transaction.Description, savedTransaction.Description)
 	assert.Equal(t, transaction.Value, savedTransaction.Value)
 	assert.True(t, transaction.Date.Equal(savedTransaction.Date))
+	assert.Equal(t, transaction.Currency, savedTransaction.Currency)
 }
